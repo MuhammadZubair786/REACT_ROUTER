9aa8a225aa81176be951ffc9fde7019c4f9e605c
Muhammad Zubair <57016162+MuhammadZubair786@users.noreply.github.com>
2020-11-27 01:17:47 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router,Route,} from "react-router-dom";
+import Home from './Components/Home'
+import About from './Components/About'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <Router>
+          <Route path='/' exact component={Home}/>
+          <Route path='/about' exact component={About}/>
+
+
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
